0a5a8e222e3203ea371d20f91328db32d2c5916d
Tanaka Lusengo <79418686+tanaka-lusengo@users.noreply.github.com>
2022-05-20 21:49:42 +0100

    add blazers products list page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Navigation from "./components/Navigation/Navigation";
 import Advert from "./components/Advert";
+import ProductList from "./pages/ProductList";
 
 const App = () => {
   return (
@@ -12,7 +13,7 @@ const App = () => {
       <Navigation />
       <Switch>
         <Redirect from="/home" to="/" />
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={ProductList} />
       </Switch>
     </div>
   );
